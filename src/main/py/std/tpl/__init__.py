4ea7925586b9e53431768f5f9f1fc0af43e5f1f3
Adrien H.L <happyman@hotmail.fr>
2022-04-17 20:21:20 +0200

    [WIP] Today's weirdness is tomorrow's reason why. 		-- Hunter S. Thompson

--- src/main/py/std/tpl/__init__.py
+++ src/main/py/std/tpl/__init__.py
@@ -3,20 +3,26 @@
 # coding: utf-8
 
 from __future__ import unicode_literals
-
+from exceptions.loggers import setup_logging_pre
 __license__ = 'Public Domain'
 
 import codecs
 import io
+import logging
 import os
 import random
 import sys
+
+
+# logger = logging.getLogger('Main')
+logger = logging.getLogger(__name__)
 
 def _real_main(argv=None):
   pass
 
 def main(argv=None):
     try:
+        setup_logging_pre()
         _real_main(argv)
     except LoadError:
         sys.exit(1)
@@ -24,5 +30,17 @@ def main(argv=None):
         sys.exit('ERROR: fixed output name but more than one file to download')
     except KeyboardInterrupt:
         sys.exit('\nERROR: Interrupted by user')
+    except SystemExit as e:  # pragma: no cover
+        return_code = e
+    except KeyboardInterrupt:
+        logger.info('SIGINT received, aborting ...')
+        return_code = 0
+    except FreqtradeException as e:
+        logger.error(str(e))
+        return_code = 2
+    except Exception:
+        logger.exception('Fatal exception!')
+    finally:
+        sys.exit(return_code)
 
 __all__ = ['main', 'Load', 'gen_extractors', 'list_extractors']
--- src/main/py/std/tpl/__main__.py
+++ src/main/py/std/tpl/__main__.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 # $ python <project-folder>/__main__.py (2.6+)
 # $ python -m project-name          (2.7+)
 
+import project
 import sys
 
 if __package__ is None and not hasattr(sys, 'frozen'):
@@ -12,8 +13,6 @@ if __package__ is None and not hasattr(sys, 'frozen'):
     import os.path
     path = os.path.realpath(os.path.abspath(__file__))
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
-
-import project
 
 if __name__ == '__main__':
     project.main()
